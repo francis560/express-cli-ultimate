ac4ac97f50f7e1e348193d34206f59c75758f201
francis560 <francispinales94@gmail.com>
2022-06-13 10:41:06 -0400

    Cli funcionando

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ExpressJs cli
+An easier way to build your project structure in express.
+
+## Requirements
+node v16
+
+## Dependencies
+* express
+* morgan
--- app/app.js
+++ app/app.js
@@ -3,51 +3,69 @@
 import inquirer from "inquirer";
 import path from "path";
 import shell from "shelljs";
-import fs, { copyFile } from "fs";
-import chalkAnimation from "chalk-animation";
+import fs from "fs";
+import gradient from "gradient-string"; 
 import { createSpinner } from "nanospinner";
 import { fileURLToPath } from "url";
+import welcome from "./welcome.js";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 
+welcome();
+// {
+//     type: "confirm",
+//     name: "database",
+//     message: "Do you want to use a database?"
+// }
 
-console.clear();
+inquirer.prompt([{
+    type: "input",
+    name: "name",
+    message: "What is the name of your project?",
+    default: "test-project"
+}, {
+    type: "list",
+    name: "type",
+    message: "Select your type of project",
+    choices: ["javascript"]
+}]).then(({ name, type, database }) => {
 
+    const spinner = createSpinner(gradient.rainbow("Creating...")).start();
 
-const createProject = () => {
-
-    inquirer.prompt([{
-        type: "input",
-        name: "name",
-        message: "What is the name of your project?",
-        default: "test-project"
-    }, {
-        type: "list",
-        name: "type",
-        message: "Select your type of project",
-        choices: ["javascript", "typescript"]
-    }, {
-        type: "confirm",
-        name: "database",
-        message: "Do you want to use a database?"
-    }]).then(({ name, type, database }) => {
+    try {
     
-        const spinner = createSpinner("Creating...").start();
+        fs.mkdir(path.join(process.cwd(), `/${name}/`), (err) => {
+
+            if (err) {
+                spinner.error({text: `The name ${name} is in use.`});
     
-        
-        fs.mkdir(path.join(__dirname, `../../${name}/`), (err) => spinner.error({text: `The name ${name} is in use.`}));
-        
-        shell.cp("-R", path.join(__dirname, `/template/${type}/*`), path.join(__dirname, `../../${name}`));
-        
-        if (database) {
-            fs.writeFile(path.join(__dirname, `../../${name}/config/ database.js`), "hola desde config" , (err) => console.error(err));
-        }
-        
-        spinner.success({text: chalkAnimation.rainbow("Project created successfully."), mark: ""});
-    });
+                throw new Error();
+            }
 
-}
+        });
+        
+        shell.cp("-R", path.join(process.cwd(), `app/template/${type}`), path.join(process.cwd(), `/${name}`));
+        
+        // if (database) {
+        //     fs.writeFile(path.join(__dirname, `../../${name}/config/ database.js`), "hola desde config" , (err) => {
+                
+        //         if (err) {
+        //             throw new Error();
+        //         }
 
+        //     });
+        // }
+        
+        spinner.success({text: gradient.rainbow("Project created successfully.")});
 
-createProject();
+        
+
+    } catch (err) {
+
+        console.error(err);
+
+    }
+    
+
+});
