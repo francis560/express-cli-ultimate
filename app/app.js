f18e63357e1c774e16fd4c16e606e3ac7efff3ce
francis560 <francispinales94@gmail.com>
2022-06-19 11:36:47 -0400

    Integrando commander js

--- app/app.js
+++ app/app.js
@@ -1,4 +1,3 @@
-#!/usr/bin/env node
 import inquirer from "inquirer";
 import shell from "shelljs";
 import gradient from "gradient-string"; 
@@ -9,64 +8,70 @@ import finalMessage from "./finalProjectMessages.js";
 import "./commands.js";
 
 
-welcome();
+const generateProject = () => {
 
-
-inquirer.prompt([{
-    type: "input",
-    name: "name",
-    message: "What is the name of your project?",
-    default: "test-project"
-}, {
-    type: "list",
-    name: "type",
-    message: "Select your type of project",
-    choices: ["javascript", "typescript"]
-} 
-// {
-//     type: "confirm",
-//     name: "database",
-//     message: "Do you want to use a database?"
-// }
-]).then(({ name, type, database }) => {
-
-    try {
-
-        console.log("\n");
-
-        shell.config.silent = true;
-
-        // if (database) {
-        //     inquirer.prompt({
-        //         type: "list",
-        //         name: "database_type",
-        //         message: "Select your database type",
-        //         choices: ["mongodb"]
-        //     }).then(({ database_type }) => {
-
-
-
-        //     });
-
-        //     return;
-        // } 
-        
-        
-        const spinner = createSpinner(gradient.rainbow(`Creating a new Express project in ${process.cwd()} \n`)).start();
-        
-        createFolderProject(name);
-
-        createBasicFiles(name, type);
-        
-        spinner.stop();
-
-        finalMessage(name, spinner);
-
-    } catch (err) {
-
-        console.error(err);
-
-    }
+    welcome();
     
+    inquirer.prompt([{
+        type: "input",
+        name: "name",
+        message: "What is the name of your project?",
+        default: "test-project"
+    }, {
+        type: "list",
+        name: "type",
+        message: "Select your type of project",
+        choices: ["javascript", "typescript"]
+    } 
+    // {
+    //     type: "confirm",
+    //     name: "database",
+    //     message: "Do you want to use a database?"
+    // }
+    ]).then(({ name, type, database }) => {
+    
+        try {
+    
+            console.log("\n");
+    
+            shell.config.silent = true;
+    
+            // if (database) {
+            //     inquirer.prompt({
+            //         type: "list",
+            //         name: "database_type",
+            //         message: "Select your database type",
+            //         choices: ["mongodb"]
+            //     }).then(({ database_type }) => {
+    
+    
+    
+            //     });
+    
+            //     return;
+            // } 
+            
+            
+            const spinner = createSpinner(gradient.rainbow(`Creating a new Express project in ${process.cwd()} \n`)).start();
+            
+            createFolderProject(name);
+    
+            createBasicFiles(name, type);
+            
+            spinner.stop();
+    
+            finalMessage(name, spinner);
+    
+        } catch (err) {
+    
+            console.error(err);
+    
+        }
+        
+    
+    });
 
-});
+}
+
+
+export default generateProject;
