0c149734f88d3b15d27f8e7c3c7f7119f249f1d3
francis560 <francispinales94@gmail.com>
2022-06-20 21:48:02 -0400

    Preparando el generador de rutas

--- app/commands.js
+++ app/commands.js
@@ -6,13 +6,11 @@ import { generateRoute } from "./config/generateRoutes.js";
 
 program.name("express-cli").version("0.2.0").description("An easier way to build your project structure in express.");
 
-// program.option("-t, --type <type>", "Type of file");
 
 program.command("new").description("Generate project").action(() => {
     generateProject();
 });
 
-// program.option("-r, --router <name>", "Generate router file");
 
 program.command("generate").alias("g").description("Generate file").option("-r, --route <name>", "Generate router file").option("-t, --type <type>", "Type of file").action((args) => {
     generateRoute(args);
--- app/config/generateRoutes.js
+++ app/config/generateRoutes.js
@@ -1,14 +1,14 @@
+import path from "path";
 import shell from "shelljs";
 
 
 export const generateRoute = (args) => {
-    
-    if (args.route) {
-        
-        args.type === "ts" ? console.log("ts") : console.log("js")
 
-        console.log(args.route);
-        
+    if (args.route) {
+
+        shell.touch(path.join(process.cwd(), args.type === "ts" || "typescript" ? `/app/routes/${args.route}.routes.ts` : `/app/routes/${args.route}.routes.js`));
+        shell.touch(path.join(process.cwd(), args.type === "ts" || "typescript" ? `/app/controllers/${args.route}.controller.ts` : `/app/controllers/${args.route}.controller.js`));
+                
     }
 
 }
